e07f8c9b277d2a986cf45ef3ec16e41f2b0e99fc
dkb3tb <55751781+dkb3tb@users.noreply.github.com>
2020-04-26 15:25:11 -0400

    Finished
    added: hyperlink style, tcmExp.html, hohmann.html
    disabled controls buttons after their respective questions
    created multiple hidden input fields
    added changeScene2 for quicker changes
    general orthography fixes
    minor game over fixes

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function toLaunch(URL) {
 function playMusic(id) {
 
     var myMusic= document.getElementById(id);
-    //myMusic.play();
+    myMusic.play();
 
 }
 
@@ -30,6 +30,9 @@ function launch() {
 function changeScene(URL){
     setTimeout( function() { window.location = URL }, 6000 );
 }
+function changeScene2(URL){
+    window.location = URL;
+}
 
 function narrateLaunch(){
 
@@ -51,7 +54,7 @@ function narrateDock(){
         " get help from Mission Control immediately.";
     var s2 = "Now that you've made it into Earth's orbit, you should rendezvous with the ISS " +
         "to pick up extra fuel and supplies";
-    var s3 = "Say something intelligent about mass ratio or something here.";
+    var s3 = "First, we need to get into position.";
     var s4 = "When you see the ISS come into view, use the controls to fire your boosters.";
 
     var text = document.getElementById("text");
@@ -62,6 +65,7 @@ function narrateDock(){
 }
 
 function dockISS(){
+    console.log("dock iss called");
     var iss = document.getElementById("viewObject");
 
 
@@ -90,80 +94,230 @@ function makeControls(){
     button2.value = "Thrust Backward";
 
     button1.onclick = function () {
+        button1.onclick = null;
+        button2.onclick = null;
         window.location = 'dockExp.html';
-    }
-    button2.onclick = questionOne;
+    };
+
+    button2.onclick = function () {
+        button1.onclick = null;
+        button2.onclick = null;
+        document.getElementById("viewObject").src = "images/placeholder.png";
+        hohmann();
+    };
 }
 
-function explainDock(){
-    var s = "Oh no! The ISS passed right under you! Orbital mechanics plagued early attempts at docking spacecraft, " +
-        "most notably in the failed rendezvous of Gemini IV. James McDivitt also thought to thrust forward in order " +
-        "" +
-        "to rendezvous with the Gemini booster, sending it away and underneath his spacecraft.\n" +
-        "\n" +
-        "Because you are in orbit around the Earth, when you fire your thrusters, you are increasing your velocity, " +
-        "sending you into a higher phasing orbit. A phasing orbit is a kind of Hohmann Transfer Orbit, adjusting " +
-        "the orbital altitude of a spacecraft.\n" +
-        "\n" +
-        "In this higher orbit, the true anomaly of the spacecraft changes much more slowly due to the Conservation " +
-        "of Angular Momentum. This causes the linear distance between you and the target to increase, since its " +
-        "true anomaly is changing much faster than yours.\n" +
-        "\n" +
-        "In order to catch up with the ISS, you actually need to slow down! This puts you on a lower phasing orbit," +
-        " speeding up the rate of change of your true anomaly relative to the target. Eventually, you and the " +
-        "target will have roughly the same true anomaly, so you can fire your thrusters to move back to the " +
-        "higher orbit and rendezvous with the ISS.\n";
+function hohmann() {
 
-    var text = document.getElementById("dockText");
-    text.innerHTML = s;
-}
-
-function questionOne() {
-
-    var q1 = "Here's where the first question goes!";
+    var q1 = "Alright, Commander, it's time to leave Earth's orbit. The computer systems will do the hard part of " +
+        "calculating the delta-v for the Hohmann transfer, but we'll need to enter a few parameters first. Enter " +
+        "the semimajor axis of the transfer orbit's ellipse in AU.";
 
     var text = document.getElementById("text");
     text.innerHTML = q1;
 
-    var inputBox = document.getElementById("input2");
-    var answer = 'answer';
+    var inputho = document.getElementById("input2");
 
-    document.getElementById("input2")
-        .addEventListener("keyup", function(event) {
+    inputho.addEventListener("keyup", function(event) {
                 event.preventDefault();
-                if (event.keyCode === 13 && input2.value === "answer") {
-                    text.innerHTML = "correct!";
-                    setTimeout(questionTwo, 3500);
+
+                if(event.code === "Enter" && isNaN(inputho.value)){
+                    text.innerHTML = "Input a value!";
                 }
-                else if (event.keyCode === 13 && input2.value !== "answer") {
-                    // game over somehow
+                else if (event.code === "Enter" && (parseFloat(inputho.value) >= 1.76 && parseFloat(inputho.value) <= 1.77)) {
+                    inputho.value = "";
+                    inputho.parentNode.removeChild(inputho);
+                    tcm();
+                }
+                else if (event.code === "Enter" && (parseFloat(inputho.value) < 1.76 || parseFloat(inputho.value) > 1.77)) {
+                    window.location = 'hohmannExp.html';
                 }
             }
         )
-
 }
 
-function questionTwo() {
+function tcm() {
 
-    var q2 = "Here's where the second question goes!";
+    var q2 = "In order to stay on target for Mars, we're going to need to perform a trajectory correction maneuver." +
+        " We'll accomplish this with "
+    + "a turn. We'll need to use rotational dynamics in order to find our engine's necessary force to turn. Using " +
+    "Newton's second law, if the moment of inertia about the roll axis is 5 and the angular acceleration is 10, find "
+    + "The resulting force.";
 
     var text = document.getElementById("text");
     text.innerHTML = q2;
 
-    var inputBox = document.getElementById("input2");
-    var answer = 'answer2';
+    var inputtcm = document.getElementById("input4");
 
-    document.getElementById("input2")
+
+
+    inputtcm
         .addEventListener("keyup", function(event) {
                 event.preventDefault();
-                if (event.keyCode === 13 && input2.value === "answer2") {
-                    text.innerHTML = "correct again! nice!"
-                    // setTimeout(questionThree, 3500);
+
+                if(event.code === "Enter" && isNaN(inputtcm.value)){
+                    text.innerHTML = "Input a value!";
                 }
-                else if (event.keyCode === 13 && input2.value !== "answer2") {
-                    // game over somehow
+                else if (event.code === "Enter" && parseInt(inputtcm.value) === 50) {
+                    inputtcm.value = "";
+                    inputtcm.parentNode.removeChild(inputtcm);
+                    narrateEmergency();
+                }
+                else if (event.code === "Enter" && parseInt(inputtcm.value) !== 50) {
+                    window.location = 'tcmExp.html';
                 }
             }
         )
+}
+
+function narrateEmergency(){
+    var s1 = "For the love of Pluto! Something has gone wrong! " +
+        "None of the crew knows what to do, so you have to ask Mission Control for help!";
+    var s2  = "Type the answer into the text area and press 'Enter' when you get it. Ask for help before the timer runs out!";
+    var text = document.getElementById("text");
+
+    text.innerHTML = s1;
+    setTimeout(function () {text.innerHTML = s2}, 4000);
+    makeEmergencyControls();
+}
+
+function makeEmergencyControls(){
+
+    var text = document.getElementById("text");
+
+    var button1 = document.getElementById("input1");
+    var button2 = document.getElementById("input3");
+    var input = document.getElementById("input5");
+
+    var answer = "Houston, we have a problem.";
+
+    input.addEventListener("keypress", function(event) {
+
+            if (event.code === 'Enter'  && input.value === answer) {
+                clearInterval(id);
+                input.value = "";
+                countdown.innerHTML = "";
+                input.parentNode.removeChild(input);
+                button1.onclick = null;
+                button2.onclick = null;
+                narrateLanding();
+            }
+            else if (event.code === 'Enter' && input.value !== "answer") {
+                window.location = 'delayExp.html';
+            }
+        }
+    );
+
+
+    button1.value = "            ";
+    button2.value = "Mission Control";
+
+    var react;
+
+    button1.onclick = function () { };
+
+    button2.onclick = function () {
+        react = time;
+        text.innerHTML = "Waiting for a reply...This could be a while."
+        console.log(react);
+    };
+
+
+    var countdown = document.getElementById("countdown");
+
+    var time = 40;
+    var delay = 30;
+    var id = setInterval(frame, 1000);
+
+    function frame() {
+
+        if (time === 0) {
+            window.location = 'delayExp.html';
+            clearInterval(id);
+        } else {
+            setTimeout(function () {
+                time--;
+                react--;
+                delay--;
+            }, 1000);
+        }
+        countdown.innerHTML = time.toString();
+
+        if(delay === 0 && time > 0){
+            text.innerHTML = "The answer is: \n" + answer;
+        }
+    }
+}
+
+
+
+function enterMars() {
+    var mars = document.getElementById("viewObject");
+    mars.src = "images/mars.png";
+    var size = 0;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+        if (size === 300) {
+            clearInterval(id);
+        } else {
+            size++;
+            mars.style.width = size + 'px';
+        }
+    }
+
+
+}
+
+function narrateLanding(){
+
+    enterMars();
+
+    var text = document.getElementById("text");
+
+    var s1 = "Congradulations, Commander! You've arrived at Mars! All that's left is the landing. " +
+        "Input your desired angle of entry into the telemetry input bar so that you may enter the Martian " +
+        "atmosphere.";
+    var s2 = "Be careful not to be too steep or shallow. You're Entering the atmosphere at thousands " +
+        "of miles an hour, so if you're too steep, you'll burn up, but if you're too shallow, you skip right off.";
+
+    setTimeout(function(){text.innerHTML = s1}, 5000);
+    setTimeout(function () {text.innerHTML = s2}, 10000);
+    makeLandingControls();
+}
+
+function makeLandingControls() {
+
+    var text = document.getElementById("text");
+
+    var button1 = document.getElementById("input1");
+    var button2 = document.getElementById("input3");
+    var input = document.getElementById("input6");
+
+    button1.value = "            ";
+    button2.value = "            ";
+
+    var correct;
+
+    input.addEventListener("keypress", function(event) {
+
+            if(isNaN(input.value)){
+                text.innerHTML = "Input an integer value!";
+            }else{
+                if(parseInt(input.value) >= 11 && parseInt(input.value) <= 13){
+                    correct = true;
+                }else{
+                    correct = false;
+                }
+            }
+
+            if (event.code === 'Enter'  && correct === true) {
+                window.location = 'victory.html';
+            }
+            else if (event.code === 'Enter' && correct === false) {
+                window.location = 'landingExp.html';
+            }
+        }
+    )
 
 }
